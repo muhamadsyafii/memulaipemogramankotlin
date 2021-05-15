d3e8c5e84fc9a2d9cda8fddae8c380290db7fd89
Muhamad Syafii <muhamadsyafii@192.168.1.13>
2021-05-15 21:28:25 +0700

    add while and do while

---
+++ Control Flow/While dan Do While/src/Task.kt
@@ -0,0 +1,16 @@
+// type your solution here
+fun main() {
+    println("=While : ")
+    var counter = 1
+    while (counter <= 7){
+        println("Hello, World!")
+        counter++
+    }
+
+    println()
+    println("=Do While : ")
+    do {
+        println("Hello, World!")
+        counter++
+    }while (counter <= 7)
+}
