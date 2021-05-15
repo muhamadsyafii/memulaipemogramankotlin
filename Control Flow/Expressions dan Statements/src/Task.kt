aa83b08ee15b14c8979817c4c8f3aafb161ebfb7
Muhamad Syafii <muhamadsyafii@192.168.1.13>
2021-05-15 20:51:38 +0700

    add Expression and Statements

--- Control Flow/Expressions dan Statements/src/Task.kt
+++ Control Flow/Expressions dan Statements/src/Task.kt
@@ -1,1 +1,15 @@
 // type your solution here
+fun main() {
+    println("=Expression status Office hours :")
+    val openOffice = 7
+    val now = 8
+    val office = if(now > openOffice) "Office already open" else "Office closed"
+    println(office)
+
+    println()
+    println("=Expression with sum : ")
+    sum(1, 2)
+
+}
+
+fun sum(value1 : Int, value2 : Int) = println("Value : ${value1 + value2}")
